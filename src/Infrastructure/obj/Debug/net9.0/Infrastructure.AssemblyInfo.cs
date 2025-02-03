67c81b0f252215fad25b44010baf3ee36b369420
hikmetkutuk <hikmetkutuk@gmail.com>
2025-02-03 14:19:13 +0300

    [Feature]: Build project in docker

--- src/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ src/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7203671e65de3942936fcd91b741eaa42b69c29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1e146e19cb99bd56bd26a0f6f81c6da332297aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
