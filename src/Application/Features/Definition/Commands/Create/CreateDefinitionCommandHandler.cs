daeace586e71eb860277fb2de8a70a1da0cee6de
hikmetkutuk <hikmetkutuk@gmail.com>
2025-02-06 17:26:54 +0300

    [Feature]: CreateDefinitionCommandHandler içine cache güncellemesi eklendi

--- src/Application/Features/Definition/Commands/Create/CreateDefinitionCommandHandler.cs
+++ src/Application/Features/Definition/Commands/Create/CreateDefinitionCommandHandler.cs
@@ -1,10 +1,11 @@
 using Application.Common.Interfaces;
 using Application.Common.Models.Responses;
+using Application.Features.Definition.Queries.GetByType;
 using MediatR;
 
 namespace Application.Features.Definition.Commands.Create;
 
-public sealed class CreateDefinitionCommandHandler(IApplicationDbContext dbContext)
+public sealed class CreateDefinitionCommandHandler(IApplicationDbContext dbContext, IRedisCache redisCache)
     : IRequestHandler<CreateDefinitionCommand, ResponseDto<Guid>>
 {
     public async Task<ResponseDto<Guid>> Handle(CreateDefinitionCommand request, CancellationToken cancellationToken)
@@ -13,8 +14,31 @@ public sealed class CreateDefinitionCommandHandler(IApplicationDbContext dbConte
 
         dbContext.Definitions.Add(definition);
 
-        await dbContext.SaveChangesAsync(cancellationToken);
+        var cacheKey = $"definition:type:{request.Type}";
 
-        return ResponseDto<Guid>.Success(definition.Id, "Definition created successfully.");
+        try
+        {
+            await dbContext.SaveChangesAsync(cancellationToken);
+
+            var cachedData = await redisCache.GetAsync<List<GetDefinitionByTypeDto>>(cacheKey);
+
+            if (cachedData != null)
+            {
+                cachedData.Add(new GetDefinitionByTypeDto
+                {
+                    Id = definition.Id,
+                    Name = definition.Name
+                });
+
+                await redisCache.SetAsync(cacheKey, cachedData, TimeSpan.FromMinutes(10));
+            }
+
+
+            return ResponseDto<Guid>.Success(definition.Id, "Definition created successfully.");
+        }
+        catch (Exception ex)
+        {
+            throw new ApplicationException("An error occurred while creating the definition.", ex);
+        }
     }
 }
--- src/Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ src/Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13f79990e57ba42776dde0da2335977fb0def6e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b497881e78afb32dfdfa66f5d33d5ec9d4dbe1b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
